ea9608ee4ce2351a8daa93ed95f80b800beec986
Spencer Krum <nibz@spencerkrum.com>
2022-06-03 19:00:38 -0500

    improve verify_tag script

--- .github/scripts/verify_tag.sh
+++ .github/scripts/verify_tag.sh
@@ -2,4 +2,4 @@
 
 ver=$(cat pyproject.toml | egrep --color=auto -m 1 '^version' | cut -d " " -f 3 | tr -d '"')
 
-[ "${GITHUB_REF}" -eq "refs/tags/${ver}" ] || exit 1
+[ "${GITHUB_REF}" = "refs/tags/${ver}" ] || exit 1
